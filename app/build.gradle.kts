29235afbe1fb005f2bc41e786111f2bc74ec30fe
Martin Feldsztejn <martin.feldsztejn@ARFVFY40T8HV2J.localdomain>
2020-06-21 10:45:52 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.3"
+
+    defaultConfig {
+        applicationId = "com.mfeldsztejn.ringtest"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+val roomVersion = "2.2.5"
+val koinVersion = "2.1.5"
+val navigationVersion = "2.3.0-rc01"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${KotlinCompilerVersion.VERSION}")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.0-alpha06")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    // DI
+    implementation("org.koin:koin-android:$koinVersion")
+    implementation("org.koin:koin-androidx-viewmodel:$koinVersion")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+
+    // Async
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+
+}
---
+++ app/src/main/java/com/mfeldsztejn/ringtest/ui/main/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.mfeldsztejn.ringtest.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
